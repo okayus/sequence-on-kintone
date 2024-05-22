b76dc67c6f87e9751aa0543656f87dc274c9029d
okayus <toshiaki.mukai.9981@gmail.com>
2024-05-22 13:47:23 +0900

    feat:とりあえずハードコーディングでシーケンス図が表示されるようにする

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,22 +1,42 @@
 (function (PLUGIN_ID) {
-  kintone.events.on('app.record.index.show', () => {
-    const spaceEl = kintone.app.getHeaderSpaceElement();
-    if (spaceEl === null) {
-      throw new Error('The header element is unavailable on this page.');
-    }
+  'use strict';
 
-    const fragment = document.createDocumentFragment();
-    const headingEl = document.createElement('h3');
-    const messageEl = document.createElement('p');
+  // Mermaid.jsの初期化
+  window.mermaid.initialize({ startOnLoad: false });
 
-    const config = kintone.plugin.app.getConfig(PLUGIN_ID);
-    messageEl.textContent = config.message;
-    messageEl.classList.add('plugin-space-message');
-    headingEl.textContent = 'Hello kintone plugin!';
-    headingEl.classList.add('plugin-space-heading');
-
-    fragment.appendChild(headingEl);
-    fragment.appendChild(messageEl);
-    spaceEl.appendChild(fragment);
+  kintone.events.on('app.record.detail.show', async (event) => {
+    const sequenceDiagram = generateSequenceMermaid(event);
+    const content = document.querySelector('#user-js-sequence-diagram');
+    const diagram = document.createElement('div');
+    diagram.id = 'diagram';
+    diagram.className = 'mermaid';
+    const { svg } = await mermaid.render('diagram', sequenceDiagram);
+    diagram.innerHTML = svg;
+    content.appendChild(diagram);
   });
+  const participantTable = "対象者リスト";
+  const messageTable = "手順";
+
+  function generateSequenceMermaid(event) {
+    const record = event.record;
+    const participants = record[participantTable].value.map((participant) => participant.value['対象者'].value);
+    // mermaid記法に変換
+    let mermaidText = 'sequenceDiagram\n';
+    participants.forEach((participant) => {
+      mermaidText += `participant ${participant}\n`;
+    });
+    // メッセージの追加
+    const messages = record[messageTable].value.map((message) => {
+      const from = message.value['誰が'].value;
+      const to = message.value['誰に'].value;
+      const text = message.value['何をする'].value;
+      // toがない場合はNoteとして表示
+      if (to === '') {
+        return `Note over ${from}: ${text}`;
+      }
+      return `${from}->>${to}: ${text}`;
+    });
+    mermaidText += messages.join('\n');
+    return mermaidText;
+  }
 })(kintone.$PLUGIN_ID);
